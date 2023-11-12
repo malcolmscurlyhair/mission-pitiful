121d62cd10d5854e90ceaf42bcd2caf0b685a5e1
Malcolm <malcolm@chargehound.com>
2023-11-12 08:30:24 -0800

    Custom error page.

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -1,5 +1,30 @@
-<script>
-  import { page } from '$app/stores';
-</script>
-CUSTOM ERROR
-<h1>{$page.status}: {$page.error.message}</h1>
+<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+        <div>
+          <div class="absolute right-0 top-0 pr-4 pt-4 block">
+            <a href="/" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="sr-only">Back</span>
+              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </a>
+          </div>
+          <div class="mt-3 text-center sm:mt-5">
+            <p class="mt-2 mb-10 text-sm text-gray-900 mb-3 font-bold">
+              This page cannot be found.
+            </p>
+            <div class="mt-10 mb-3">
+              <a href="/" class="rounded-full bg-gray-300 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+                Get back on track →
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
