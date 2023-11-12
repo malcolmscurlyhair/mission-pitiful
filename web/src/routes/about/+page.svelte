989df81ed5044bd153305e0d4579c315f49bb27a
Malcolm <malcolm@chargehound.com>
2023-11-12 08:30:10 -0800

    Removed unnecessary markup

--- web/src/routes/about/+page.svelte
+++ web/src/routes/about/+page.svelte
@@ -1,14 +1,4 @@
 <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-  <!--
-    Background backdrop, show/hide based on modal state.
-
-    Entering: "ease-out duration-300"
-      From: "opacity-0"
-      To: "opacity-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100"
-      To: "opacity-0"
-  -->
   <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
   <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
